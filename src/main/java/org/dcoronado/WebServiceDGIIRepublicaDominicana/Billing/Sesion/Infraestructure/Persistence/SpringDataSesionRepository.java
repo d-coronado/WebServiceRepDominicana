9a020546f5b359595e6ad839019f842efbfe22d2
Davis Coronado Albines <daviscoronadoalbines@gmail.com>
2025-10-21 16:41:22 -0500

    fix(sesion) : se corrigio la consulta que obtiene una sesion activa

--- src/main/java/org/dcoronado/WebServiceDGIIRepublicaDominicana/Billing/Sesion/Infraestructure/Persistence/SpringDataSesionRepository.java
+++ src/main/java/org/dcoronado/WebServiceDGIIRepublicaDominicana/Billing/Sesion/Infraestructure/Persistence/SpringDataSesionRepository.java
@@ -10,8 +10,19 @@ import java.util.Optional;
 
 public interface SpringDataSesionRepository extends JpaRepository<SesionEntity, Long> {
 
-    String FIND_SESION_ACTIVA = "SELECT s.* FROM sesionempresa s WHERE s.sesionempresa_rnc = :rnc AND s.sesionempresa_ambiente = :ambienteEnum AND s.sesionempresa_tokenexpira > :ahora ORDER BY s.sesionempresa_tokenexpedido DESC LIMIT 1";
+    String FIND_SESION_ACTIVA = """
+            SELECT s
+            FROM SesionEntity s
+            WHERE s.rnc = :rnc
+              AND s.ambiente = :ambiente
+              AND s.expira > :ahora
+            ORDER BY s.expedido DESC
+            """;
 
-    @Query(value = FIND_SESION_ACTIVA, nativeQuery = true)
-    Optional<SesionEntity> findSesionActivaByRnc(@Param("rnc") String rnc, @Param("ambienteEnum") AmbienteEnum ambiente, @Param("ahora") LocalDateTime ahora);
+    @Query(FIND_SESION_ACTIVA)
+    Optional<SesionEntity> findSesionActivaByRnc(
+            @Param("rnc") String rnc,
+            @Param("ambiente") AmbienteEnum ambiente,
+            @Param("ahora") LocalDateTime ahora
+    );
 }
