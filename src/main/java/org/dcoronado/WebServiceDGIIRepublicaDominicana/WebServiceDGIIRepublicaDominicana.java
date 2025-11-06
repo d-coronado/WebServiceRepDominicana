df80fc05e150c3218344956866512b981eee2674
Davis Coronado Albines <daviscoronadoalbines@gmail.com>
2025-11-06 12:38:40 -0500

    Add(Config): configuracion de logs

--- src/main/java/org/dcoronado/WebServiceDGIIRepublicaDominicana/WebServiceDGIIRepublicaDominicana.java
+++ src/main/java/org/dcoronado/WebServiceDGIIRepublicaDominicana/WebServiceDGIIRepublicaDominicana.java
@@ -1,4 +1,5 @@
 package org.dcoronado.WebServiceDGIIRepublicaDominicana;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -6,6 +7,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class WebServiceDGIIRepublicaDominicana {
+    static {
+        // Forzar uso de Xerces como SAX parser
+        System.setProperty(
+                "javax.xml.parsers.SAXParserFactory",
+                "org.apache.xerces.jaxp.SAXParserFactoryImpl"
+        );
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebServiceDGIIRepublicaDominicana.class, args);
     }
